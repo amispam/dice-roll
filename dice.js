e272afbb0c8578927c247229373cab4a70a7d228
Rohit Yadav <74447976+amispam@users.noreply.github.com>
2023-06-06 18:27:30 +0530

    Update dice.js

--- dice.js
+++ dice.js
@@ -4,17 +4,6 @@ window.onload = function(){
   var player1 = parseInt((Math.random()*6)) + 1;
   var player2 = parseInt((Math.random()*6)) + 1;
 
-  if(player1 !== player2){
-    diceRolled();
-  }
-  else if(player1 === player2){
-    while(player1 === player2){
-      player2 = parseInt((Math.random()*6)) + 1;
-      player1 = parseInt((Math.random()*6)) + 1;
-    }
-    diceRolled();
-  }
-
   function diceRolled(){
     document.querySelector(".dice1").innerHTML = "<img src='images/dice"+player1+".png' style='width:70%;' alt='dice'>";
     document.querySelector(".dice2").innerHTML = "<img src='images/dice"+player2+".png' style='width:70%;' alt='dice'>";
@@ -24,5 +13,9 @@ window.onload = function(){
     else if(player2 > player1){
       document.querySelector(".result").innerHTML = "<em>player two wins.</em> 🚩";
     }
+    else{
+      document.querySelector(".result").innerHTML = "<em>DRAW</em>";
+    }
   }
+  diceRolled();
 }
